51e9903fc23cc7cd1e9228ef9c07b6e3bce74ebe
Matias De Stefano <matiasdestefano97@gmail.com>
2022-01-07 21:05:54 -0300

    Añadido genericos.ts

---
+++ src/ejercicios/09-genericos.ts
@@ -0,0 +1,14 @@
+import { Producto, calculaISV } from './ejercicios/06-desestructuracion-funcion';
+/*
+    ===== Código de TypeScript =====
+*/
+
+function queTipoSoy<T>(argumento: T) {
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(123);
+let soyArray = queTipoSoy([1,2,3]);
+
+let soyTexto = queTipoSoy<string>("Prueba")
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import { Producto, calculaISV } from './ejercicios/06-desestructuracion-funcion'
 /*
     ===== Código de TypeScript =====
 */
+
